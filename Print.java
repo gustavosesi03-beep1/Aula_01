29cd4df366d2371a16a87b5971c535171682f0ea
gustavosesi03-beep1 <gustavosesi03@gmail.com>
2025-11-25 00:35:26 +0000

    teste1

---
+++ Print.java
@@ -0,0 +1,38 @@
+public class Print{
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+ 
+//println-> imprime e pula para proxima linha
+System.out.println("ola este È um exemplo de ipressao.");
+System.out.println("cada println começa em umalinha.");
+
+// print -> imprime sem quebrar a linha
+System.out.print (s: "este texto nao quebra a linha..."));
+System.out.print(s:"continua aqui na mesma linha!");
+
+// linha em branco 
+System.out.println();
+System.out.println();
+
+// misturando print e println
+System.out.print("nome:");
+System.out.println("Joao silva");
+
+System.out.print("idade:");
+System.out.println(30);
+
+System.out.print("cidade:");
+System.out.println("Goiania");
+
+// impressoes formatadas
+System.out.println();
+System.out.println("Resustado:");
+System.out.println("5+5="(5+5));
+
+// finalizado
+System.out.println();
+System.out.println("impressao concluida");
+
+    }
+}
